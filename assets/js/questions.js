9e0c1a91fcf69544e35e106bab728d4b3a7170b5
Sarika <sarika@ngoyal.com>
2019-03-20 09:47:00 -0700

    Cleaned up Code

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,52 +1,53 @@
 questions = [{
   question: "The energy used by most living beings on the earth to feed, move, and reproduce all comes from what original source?",
-  choiceList: ["the soil","the sun", "the water", "Twinkies"],
+  choiceList: ["the soil", "the sun", "the water", "Twinkies"],
   answer: 'a',
-  explaination:"The energy of the sun is captured by plants and algae by photosynthesis."
+  explaination: "The energy of the sun is captured by plants and algae by photosynthesis."
 },
 {
   question: "Which of the following is composed of nervous tissue?",
   choiceList: ["the skin", "a paper mache statue of Jerry Lewis", "the spinal cord", "the liver"],
   answer: 'c',
-  explaination:"The spinal cord is really a direct extension of the brain."
+  explaination: "The spinal cord is really a direct extension of the brain."
 
 },
 {
   question: "A leech survives by feeding off the blood of other, larger beings. What is this type of behavior known as?",
-  choiceList: ["working for the IRS", "mutualism", "parasitism","heterophagism"],
+  choiceList: ["working for the IRS", "mutualism", "parasitism", "heterophagism"],
   answer: 'c',
-  explaination:"Parasitism"
+  explaination: "Parasitism"
 },
 {
   question: "Where are you more likely to find the largest number of living organisms (including microbes) in one gallon of water?",
-  choiceList: ["Arctic Ocean","Caspian Sea", "Caribbean Sea", "Mick Jagger's swimming pool"],
+  choiceList: ["Arctic Ocean", "Caspian Sea", "Caribbean Sea", "Mick Jagger's swimming pool"],
   answer: 'a',
-  explaination:"Cold, turbulent waters harbor the most biomass, in part because oxygen dissolves more in cold water than warm water."
+  explaination: "Cold, turbulent waters harbor the most biomass, in part because oxygen dissolves more in cold water than warm water."
 },
 {
   question: "Which of the following lives in a cocoon for part of its life cycle?",
-  choiceList: [ "Dick Clark", "a mollusk","a moth","a crab"],
+  choiceList: ["Dick Clark", "a mollusk", "a moth", "a crab"],
   answer: 'c',
-  explaination:"A cocoon protects the pupa of some insects during metamorphosis."
+  explaination: "A cocoon protects the pupa of some insects during metamorphosis."
 },
 {
   question: "Why do fish swim in schools?",
   choiceList: [
-    "to get to college", "to chase away larger fish", "to confuse predators","to surround prey"
-   ],
+      "to get to college", "to chase away larger fish", "to confuse predators", "to surround prey"
+  ],
   answer: 'c',
-  explaination:"Apparently, predators must focus on just one fish in order to strike successfully. Distracted by the school, they lose focus."
+  explaination: "Apparently, predators must focus on just one fish in order to strike successfully. Distracted by the school, they lose focus."
 },
 
 {
   question: "Which of the following animals are gutless?",
-  choiceList:  ["sea cucumbers",  "salamanders",  "earthworms",  "tapeworms" ],
+  choiceList: ["sea cucumbers", "salamanders", "earthworms", "tapeworms"],
   answer: 'd',
-  explaination:"Because the tapeworm lives inside the gut of a host, it has evolved to absorb nutrients directly from outside its body."
+  explaination: "Because the tapeworm lives inside the gut of a host, it has evolved to absorb nutrients directly from outside its body."
 },
 {
   question: "What is the scientific term for a 'cold-blooded' animal?",
   choiceList: ["a homeotherm", "a prosecuting attorney", "a cryotherm", "a poikilotherm"],
   answer: 'c',
-  explaination:"Poikilotherms such as reptiles aren't actually cold. Their body temperature varies and is regulated by the environment and their behavior. Perhaps more common is the term 'ectotherm,' referring to the fact that the animals must rely on external heat."
-}]
+  explaination: "Poikilotherms such as reptiles aren't actually cold. Their body temperature varies and is regulated by the environment and their behavior. Perhaps more common is the term 'ectotherm,' referring to the fact that the animals must rely on external heat."
+}
+]
